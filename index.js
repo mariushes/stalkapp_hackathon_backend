606e60d6592739d15c1cc8a5843c87bd5da4a38d
Marius Hessenthaler <mhessent@mail.uni-mannheim.de>
2020-02-24 19:39:32 +0200

    Instagram API

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 // const FB = require("FB");
 const queryString = require("query-string");
-const http = require("http");
+const http = require("https");
 
 // example request
 // http.get("<url>", function (http_response) {
@@ -27,12 +27,56 @@ app.get('/germany', function (req, res) {
 
 
 app.get('/api/:user_name', function (req, res) {
+	var userid_url = "https://www.instagram.com/web/search/topsearch/?context=blended&query=" +
+	req.params.user_name;
+	var profile_url = "Unkown";
+	http.get(userid_url, function (http_response) {
+		http_response.on("error", function (data) {
+			console.log(error);
+		})
+		var body = "";
+		http_response.on("data", function (data) {
+			body = body + data;
+		})
+		http_response.on("end", function (data) {
+
+				const username_response = JSON.parse(body);
+				userid = username_response.users[0].user.username;
+				console.log(userid);
+				profile_url = 'https://www.instagram.com/' + userid +'?__a=1';
+				console.log(profile_url);
+
+				http.get(profile_url, function (http_response2) {
+					http_response2.on("error", function (data) {
+						console.log(error);
+					})
+					var body = "";
+					http_response2.on("data", function (data) {
+						process.stdout.write(data);
+						//body = body + data;
+					})
+					http_response2.on("end", function (data) {
+						console.log(body);
+						const profile_response = JSON.parse(body);
+						console.log(profile_response);
+
+					})
+				})
+
+		})
+	})
+	console.log(profile_url);
+
+
+});
     // search for the user name
     // url: https://www.instagram.com/web/search/topsearch/?context=blended&query=<user name
     // get the first user's username
     // build url: https://www.instagram.com/<username>?__a=1
     // get the following data: .graphql.user.edge_owner_to_timeline_media
-});
+	
+	
+
 
 // wikipedia search:
 // https://en.wikipedia.org/w/api.php?action=query&format=json&generator=prefixsearch&gpssearch=<search string>
