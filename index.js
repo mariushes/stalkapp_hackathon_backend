fc2e9da69a6a46066c709e7ab5e825c2150ff4d0
Bogdan Mic <bogdan.mic@hotmail.com>
2020-02-24 16:35:05 +0200

    Added example code

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
-const FB = require("FB");
+// const FB = require("FB");
+const queryString = require("query-string");
+const http = require("http");
+
+// example request
+// http.get("<url>", function (http_response) {
+//     http_response.on("data", function (data) {
+//         // Do something with the data
+//         // e.g. res.send(data)
+//     })
+// });
 
 function base_route(req, res) {
-    res.send('Hello World')
+    console.log(req.query.code);
+    console.log(req);
+    res.send(req.query.code)
 }
 
 
@@ -14,8 +26,18 @@ app.get('/germany', function (req, res) {
 });
 
 
-app.get('/facebook', function (req, res) {
-    res.send('Hallo Leute!')
+app.get('/api/:user_name', function (req, res) {
+    // search for the user name
+    // url: https://www.instagram.com/web/search/topsearch/?context=blended&query=<user name
+    // get the first user's username
+    // build url: https://www.instagram.com/<username>?__a=1
+    // get the following data: .graphql.user.edge_owner_to_timeline_media
 });
+
+// wikipedia search:
+// https://en.wikipedia.org/w/api.php?action=query&format=json&generator=prefixsearch&gpssearch=<search string>
+// get pages keys: first_key = Object.keys(.query.pages)[0]
+// get the first result: title = data.query.pages[first_key].title
+// build the following url: http://wikipedia.com/wiki/<title>
 
 app.listen(3000);
